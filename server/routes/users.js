e4ee5fa7ed5a67d5a166cfdb50265962b60cf0e2
William Jagels <will.jagels@gmail.com>
2015-02-07 05:49:34 -0500

    Making the api happen

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient
+, assert = require('assert');
+var url = 'mongodb://localhost:27017/test';
 
-/* GET users listing. */
+/* GET users public listing. */
 router.get('/:id', function(req, res, next) {
-  res.send(req.params.id);
-});
+	MongoClient.connect(url, function(err, db) {
+		assert.equal(null, err);
+		console.log("Connected correctly to server");
+		var usrs = db.collection('users');
+		usrs.find({"user": req.params.id}, function(err, data) {
+			res.send(data);
+		});
+	});
+}
 
 module.exports = router;
