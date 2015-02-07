cad8f669bc23754700e226777f66287735ab91e4
Michael Correale <Mcorreale1@gmail.com>
2015-02-07 17:27:38 -0500

    NVM, WE NOT DONE HERE YET. bunch of changes for upcoming database integration"
    eof
    EOF

--- server/public/javascripts/scripts.js
+++ server/public/javascripts/scripts.js
@@ -1,24 +1,42 @@
-//Fills main div to fill rest of screen
-$(document).ready(function(){
-	//Fills main div to fill rest of screen
-
-	var hgt = window.innerHeight - $(".header").height() - $(".footer").height();
-	if(hgt > $(".main".height())){
-		$(".main").css("height", hgt);
-		$(window).resize(function(){
-			hgt = window.innerHeight - $(".header").height() - $(".footer").height();
-			$(".main").css("height", hgt);
-		});
-	}
-	$('#signinbut').click(function(){
-		$("input[type=button]").addClass("off");
-		$(".signinform").addClass("on");
-	});
-});
-
 function isLoggedIn(){
 	//TODO: MAKE THIS
 	var isLogged = true;
 	//If(whatever){}
 	return isLogged;
 }
+
+//Fills main div to fill rest of screen
+$(document).ready(function(){
+	//Fills main div to fill rest of screen
+
+	var hgt = window.innerHeight - $(".header").height() - $(".footer").height();
+	if(hgt > $(".main").height()){
+		$(".main").css("height", hgt);
+		$(window).resize(function(){
+			hgt = window.innerHeight - $(".header").height() - $(".footer").height();
+			$(".main").css("height", hgt);
+		});
+	}
+	var hidden = true
+	$(".flip").click(function() {
+	
+		$(".panel").toggle(function() {
+			if(hidden) { 
+				$(".panel").show(); 
+			} else { 
+				$(".panel").hide();
+			}
+			hidden = !hidden;
+		});
+	});
+
+	if(isLoggedIn()){
+		$(".verify").text("Account Info");
+		$(".signin").hide();
+		$(".signedin").show();
+	}
+	else{
+		$(".signedin").hide();
+	}
+});
+
